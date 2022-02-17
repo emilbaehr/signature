6ef18f2dad91e6a16d0fddd5580e78ac3d2e35ba
Emil Baehr <emil.baehr@shortcut.io>
2022-02-17 15:33:58 +0100

    Updated with form completion on input change

---
+++ script.js
@@ -0,0 +1,36 @@
+const nameInput = document.getElementById("name");
+const titleInput = document.getElementById("job-title");
+const emailInput = document.getElementById("email");
+const phoneInput = document.getElementById("phone-number");
+
+trackInput(nameInput, document.getElementById("name-value"))
+trackInput(titleInput, document.getElementById("title"))
+trackInput(emailInput, document.getElementById("email-value"))
+trackInput(phoneInput, document.getElementById("phone-value"))
+
+function trackInput(inputField, outputField) {
+    inputField.addEventListener("input", () => {
+        outputField.innerHTML = inputField.value
+    });
+}
+
+function copyToClipboard(html) {
+    var container = document.createElement("div");
+    container.innerHTML = html;
+    container.style.position = "fixed";
+    container.style.pointerEvents = "none";
+    container.style.opacity = 0;
+    document.body.appendChild(container);
+    window.getSelection().removeAllRanges();
+    var range = document.createRange();
+    range.selectNode(container);
+    window.getSelection().addRange(range);
+    document.execCommand("copy");
+    document.body.removeChild(container);
+    alert("Copied");
+}
+
+document.querySelector("button").onclick = function () {
+    var htmlEditor = document.getElementById("html-to-copy");
+    copyToClipboard(htmlEditor.innerHTML);
+};
